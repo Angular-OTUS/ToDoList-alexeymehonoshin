e716680497e7546eebbdb3e6ad868e6a5a77bb49
Мехоношин Алексей Михайлович <rusdec@internet.ru>
2023-05-10 15:23:38 +0300

    [lesson-14] Добавлен environment

--- src/app/api/todo-list-api.service.ts
+++ src/app/api/todo-list-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TodoListItem } from '../interfaces/todo-list-item.interface';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments';
 
 type ItemId = TodoListItem['id'];
 type CreatedFields = Omit<TodoListItem, 'id'>
@@ -11,25 +12,25 @@ type UpdatedFields = Partial<Omit<TodoListItem, 'id'>>
   providedIn: 'root',
 })
 export class TodoListApiService {
-  private readonly API_HOST = 'http://localhost:3000';
+  private readonly API_URL = environment.todoListApiUrl;
 
   constructor(
     private httpClient: HttpClient,
   ) {}
 
   getItems(params = {}): Observable<TodoListItem[]> {
-    return this.httpClient.get<TodoListItem[]>(`${this.API_HOST}/items`, { params });
+    return this.httpClient.get<TodoListItem[]>(`${this.API_URL}/items`, { params });
   }
 
   updateItem(id: ItemId, data: UpdatedFields): Observable<TodoListItem> {
-    return this.httpClient.put<TodoListItem>(`${this.API_HOST}/items/${id}`, data);
+    return this.httpClient.put<TodoListItem>(`${this.API_URL}/items/${id}`, data);
   }
 
   deleteItem(id: ItemId): Observable<void> {
-    return this.httpClient.delete<void>(`${this.API_HOST}/items/${id}`);
+    return this.httpClient.delete<void>(`${this.API_URL}/items/${id}`);
   }
 
   createItem(data: CreatedFields): Observable<TodoListItem> {
-    return this.httpClient.post<TodoListItem>(`${this.API_HOST}/items`, data);
+    return this.httpClient.post<TodoListItem>(`${this.API_URL}/items`, data);
   }
 }
